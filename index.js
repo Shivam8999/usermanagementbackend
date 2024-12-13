4e7dd74f9dc4b740a0b0d5bb56b57ad582275bb7
Shivam8999 <king8999274125@gmail.com>
2024-12-13 20:20:01 +0530

    Initial setup completed

---
+++ index.js
@@ -0,0 +1,11 @@
+const express= require('express')
+
+const app = express()
+
+app.get("/api",(req,res) => {
+    res.send("hello world")
+})
+
+app.listen(3000,"0.0.0.0",() => {
+    console.log("server is running on port 3000")
+})
